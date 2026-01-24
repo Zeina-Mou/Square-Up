3434d7aecdbf729ba1aaca967036329ba024bf63
moufidananouh <moufidaananouh@gmail.com>
2026-01-23 21:02:57 -0500

    Edit hero and companies section

---
+++ Square-Up/src/Pages/About.jsx
@@ -0,0 +1,15 @@
+import HeaderSection from "../SharedComponents/HeaderSection/HeaderSection"
+
+const About = () => {
+  return (
+    <>
+      <HeaderSection
+        title="About Us"
+        description="Welcome to SquareUp, where collaboration, expertise, and client-centricity intersect to shape the future of digital innovation."
+        image={"assets/img/about/hero.png"}
+      />
+    </>
+  )
+}
+
+export default About
---
+++ Square-Up/src/Pages/Work.jsx
@@ -0,0 +1,15 @@
+import HeaderSection from "../SharedComponents/HeaderSection/HeaderSection"
+
+const Work = () => {
+  return (
+    <>
+      <HeaderSection
+        title="Our Works"
+        description="Discover a portfolio of visually stunning and strategically crafted digital projects that showcase our creativity and expertise."
+        image={"assets/img/works/hero.png"}
+      />
+    </>
+  )
+}
+
+export default Work
